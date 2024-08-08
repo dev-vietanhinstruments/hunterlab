ab368a6277fe6bf92239c7df0aefe736588d06ec
Thanh Lan <emeraldlan1105@gmail.com>
2024-08-08 10:58:30 +0700

    add industry display

---
+++ consts/industries.js
@@ -0,0 +1,47 @@
+export const INDUSTRIES = [
+    {
+        id: '1',
+        name: 'Đồ ăn',
+        icon: '/industries/food.svg',
+    },
+    {
+        id: '2',
+        name: 'Nước uống',
+        icon: '/industries/drink.svg',
+    },
+    {
+        id: '3',
+        name: 'Nhựa',
+        icon: '/industries/plastic.svg',
+    },
+    {
+        id: '4',
+        name: 'Hóa học',
+        icon: '/industries/chemistry.svg',
+    },
+    {
+        id: '5',
+        name: 'Dược phẩm',
+        icon: '/industries/medic.svg',
+    },
+    {
+        id: '6',
+        name: 'Xây dựng',
+        icon: '/industries/building.svg',
+    },
+    {
+        id: '7',
+        name: 'Mỹ thuật',
+        icon: '/industries/arts.svg',
+    },
+    {
+        id: '8',
+        name: 'In ấn',
+        icon: '/industries/printing.svg',
+    },
+    {
+        id: '9',
+        name: 'Dệt may',
+        icon: '/industries/sew.svg',
+    },
+]
