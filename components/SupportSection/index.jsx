946a9a47d14e5a5072db66fa938e3341113e1b58
Thanh Lan <emeraldlan1105@gmail.com>
2024-08-23 13:00:17 +0700

    change support bg

--- components/SupportSection/index.jsx
+++ components/SupportSection/index.jsx
@@ -4,7 +4,7 @@ import Section from '@/components/Layout/Section'
 
 export default function SupportSection() {
 	return (
-		<Section className='bg-card mb-0 sm:mb-0'>
+		<Section className='bg-white mb-0 sm:mb-0'>
 			<div className='relative grid gap-8 lg:gap-12 grid-cols-1 lg:grid-cols-3 w-full'>
 				{SUPPORTS.map((support, index) => (
 					<SupportCard
