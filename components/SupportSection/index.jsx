62008dd6c93fdcfdeab058906f4adbbeb76ce75e
Thanh Lan <emeraldlan1105@gmail.com>
2024-08-23 12:31:34 +0700

    fix support

--- components/SupportSection/index.jsx
+++ components/SupportSection/index.jsx
@@ -5,7 +5,7 @@ import Section from '@/components/Layout/Section'
 export default function SupportSection() {
 	return (
 		<Section className='bg-card mb-0 sm:mb-0'>
-			<div className='grid grid-flow-row gap-12 grid-cols-1 lg:grid-cols-3 w-full'>
+			<div className='grid gap-8 lg:gap-12 grid-cols-1 lg:grid-cols-3 w-full'>
 				{SUPPORTS.map((support, index) => (
 					<SupportCard
 						key={index}
